72e7f93ff4823bb700d3f21c3bf21b19b6daec9f
积木 <1820452084@qq.com>
2025-05-28 10:19:49 +0800

    fix: add uuid

--- dist/chat-robot.umd.js
+++ dist/chat-robot.umd.js
@@ -1,8 +1,8 @@
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('react'), require('react-dom'), require('../../../../../../assets/images/canal/MYSQL.svg'), require('../../../../../../assets/images/canal/Oracle.svg'), require('../../../../../../assets/images/canal/Kafka.svg'), require('../../../../../../assets/images/canal/SQLServer.svg'), require('../../../../../../assets/images/canal/MongoDB.svg'), require('../../../../../../assets/images/canal/TiDB.svg'), require('uuid'), require('../../../../../../assets/images/robot/clougence-icon.svg'), require('../../../../../../assets/images/robot/head.png'), require('../../../../../../assets/images/robot/login-bg.png'), require('../../../../../../assets/canal/wechat.png')) :
-	typeof define === 'function' && define.amd ? define(['react', 'react-dom', '../../../../../../assets/images/canal/MYSQL.svg', '../../../../../../assets/images/canal/Oracle.svg', '../../../../../../assets/images/canal/Kafka.svg', '../../../../../../assets/images/canal/SQLServer.svg', '../../../../../../assets/images/canal/MongoDB.svg', '../../../../../../assets/images/canal/TiDB.svg', 'uuid', '../../../../../../assets/images/robot/clougence-icon.svg', '../../../../../../assets/images/robot/head.png', '../../../../../../assets/images/robot/login-bg.png', '../../../../../../assets/canal/wechat.png'], factory) :
-	(global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.ChatRobot = factory(global.React, global.ReactDOM, global.MysqlIcon, global.OracleIcon, global.KafkaIcon, global.SQLServerIcon, global.MongoDBIcon, global.TiDBIcon, global.uuid, global.robotIcon, global.userAvatar, global.quickstartImage, global.wechatImage));
-})(this, (function (React, require$$1, MysqlIcon, OracleIcon, KafkaIcon, SQLServerIcon, MongoDBIcon, TiDBIcon, uuid, robotIcon, userAvatar, quickstartImage, wechatImage) { 'use strict';
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('react'), require('react-dom'), require('../../../../../../assets/images/canal/MYSQL.svg'), require('../../../../../../assets/images/canal/Oracle.svg'), require('../../../../../../assets/images/canal/Kafka.svg'), require('../../../../../../assets/images/canal/SQLServer.svg'), require('../../../../../../assets/images/canal/MongoDB.svg'), require('../../../../../../assets/images/canal/TiDB.svg'), require('../../../../../../assets/images/robot/clougence-icon.svg'), require('../../../../../../assets/images/robot/head.png'), require('../../../../../../assets/images/robot/login-bg.png'), require('../../../../../../assets/canal/wechat.png')) :
+	typeof define === 'function' && define.amd ? define(['react', 'react-dom', '../../../../../../assets/images/canal/MYSQL.svg', '../../../../../../assets/images/canal/Oracle.svg', '../../../../../../assets/images/canal/Kafka.svg', '../../../../../../assets/images/canal/SQLServer.svg', '../../../../../../assets/images/canal/MongoDB.svg', '../../../../../../assets/images/canal/TiDB.svg', '../../../../../../assets/images/robot/clougence-icon.svg', '../../../../../../assets/images/robot/head.png', '../../../../../../assets/images/robot/login-bg.png', '../../../../../../assets/canal/wechat.png'], factory) :
+	(global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.ChatRobot = factory(global.React, global.ReactDOM, global.MysqlIcon, global.OracleIcon, global.KafkaIcon, global.SQLServerIcon, global.MongoDBIcon, global.TiDBIcon, global.robotIcon, global.userAvatar, global.quickstartImage, global.wechatImage));
+})(this, (function (React, require$$1, MysqlIcon, OracleIcon, KafkaIcon, SQLServerIcon, MongoDBIcon, TiDBIcon, robotIcon, userAvatar, quickstartImage, wechatImage) { 'use strict';
 
 	var commonjsGlobal = typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
 
@@ -7149,6 +7149,70 @@
 
 	var markdownItExports = requireMarkdownIt();
 	var MarkdownIt = /*@__PURE__*/getDefaultExportFromCjs(markdownItExports);
+
+	// Unique ID creation requires a high quality random # generator. In the browser we therefore
+	// require the crypto API and do not support built-in fallback to lower quality random number
+	// generators (like Math.random()).
+	var getRandomValues;
+	var rnds8 = new Uint8Array(16);
+	function rng() {
+	  // lazy load so that environments that need to polyfill have a chance to do so
+	  if (!getRandomValues) {
+	    // getRandomValues needs to be invoked in a context where "this" is a Crypto implementation. Also,
+	    // find the complete implementation of crypto (msCrypto) on IE11.
+	    getRandomValues = typeof crypto !== 'undefined' && crypto.getRandomValues && crypto.getRandomValues.bind(crypto) || typeof msCrypto !== 'undefined' && typeof msCrypto.getRandomValues === 'function' && msCrypto.getRandomValues.bind(msCrypto);
+
+	    if (!getRandomValues) {
+	      throw new Error('crypto.getRandomValues() not supported. See https://github.com/uuidjs/uuid#getrandomvalues-not-supported');
+	    }
+	  }
+
+	  return getRandomValues(rnds8);
+	}
+
+	var REGEX = /^(?:[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}|00000000-0000-0000-0000-000000000000)$/i;
+
+	function validate(uuid) {
+	  return typeof uuid === 'string' && REGEX.test(uuid);
+	}
+
+	/**
+	 * Convert array of 16 byte values to UUID string format of the form:
+	 * XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
+	 */
+
+	var byteToHex = [];
+
+	for (var i = 0; i < 256; ++i) {
+	  byteToHex.push((i + 0x100).toString(16).substr(1));
+	}
+
+	function stringify(arr) {
+	  var offset = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+	  // Note: Be careful editing this code!  It's been tuned for performance
+	  // and works in ways you may not expect. See https://github.com/uuidjs/uuid/pull/434
+	  var uuid = (byteToHex[arr[offset + 0]] + byteToHex[arr[offset + 1]] + byteToHex[arr[offset + 2]] + byteToHex[arr[offset + 3]] + '-' + byteToHex[arr[offset + 4]] + byteToHex[arr[offset + 5]] + '-' + byteToHex[arr[offset + 6]] + byteToHex[arr[offset + 7]] + '-' + byteToHex[arr[offset + 8]] + byteToHex[arr[offset + 9]] + '-' + byteToHex[arr[offset + 10]] + byteToHex[arr[offset + 11]] + byteToHex[arr[offset + 12]] + byteToHex[arr[offset + 13]] + byteToHex[arr[offset + 14]] + byteToHex[arr[offset + 15]]).toLowerCase(); // Consistency check for valid UUID.  If this throws, it's likely due to one
+	  // of the following:
+	  // - One or more input array values don't map to a hex octet (leading to
+	  // "undefined" in the uuid)
+	  // - Invalid input values for the RFC `version` or `variant` fields
+
+	  if (!validate(uuid)) {
+	    throw TypeError('Stringified UUID is invalid');
+	  }
+
+	  return uuid;
+	}
+
+	function v4(options, buf, offset) {
+	  options = options || {};
+	  var rnds = options.random || (options.rng || rng)(); // Per 4.4, set bits for version and `clock_seq_hi_and_reserved`
+
+	  rnds[6] = rnds[6] & 0x0f | 0x40;
+	  rnds[8] = rnds[8] & 0x3f | 0x80; // Copy bytes to buffer, if provided
+
+	  return stringify(rnds);
+	}
 
 	/* eslint-disable no-multi-assign */
 
@@ -69455,7 +69519,7 @@
 	    }));
 	  }
 	  React.useEffect(() => {
-	    setSessionId(uuid.v4());
+	    setSessionId(v4());
 	    const script = document.createElement('script');
 	    script.src = 'http//g.alicdn.com/chatui/icons/2.6.2/index.js';
 	    script.async = true;
