b97c77cfd8b8b8541054505702601c10b52a6342
积木 <1820452084@qq.com>
2025-06-10 16:17:48 +0800

    fix: fix code

--- dist/chat-robot.0610_1616.js
+++ dist/chat-robot.0610_1616.js
@@ -553,7 +553,6 @@
       className: "send-button"
     }, paused ? /*#__PURE__*/React.createElement("svg", {
       t: "1749536638247",
-      "class": "icon",
       viewBox: "0 0 1024 1024",
       version: "1.1",
       xmlns: "http://www.w3.org/2000/svg",
@@ -566,7 +565,6 @@
       "p-id": "18419"
     })) : /*#__PURE__*/React.createElement("svg", {
       t: "1749536193030",
-      "class": "icon",
       viewBox: "0 0 1024 1024",
       version: "1.1",
       xmlns: "http://www.w3.org/2000/svg",
